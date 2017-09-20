6fce43b3cb225d5e5e287222efff630d3bc22cd6
Alex Levine <aalexlevine@gmail.com>
2017-09-20 18:36:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,11 +18,11 @@ function logWhisper(string) {
 function sayHiToGrandma(string) {
   if (string.toLowerCase() === string) {
     return "I can't hear you!"
-} 
+}
 else if (string.toUpperCase() === string) {
     return "YES INDEED!"
-  } 
-  else "I love you, Grandma" === string { 
+  }
+  else "I love you, Grandma" === string {
     return "I love you, too"
   }
 }
