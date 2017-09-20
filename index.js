f9cb9315b0a976535afb63f4a97deeeb843e881e
Alex Levine <aalexlevine@gmail.com>
2017-09-20 18:36:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ function sayHiToGrandma(string) {
 else if (string.toUpperCase() === string) {
     return "YES INDEED!"
   }
-  else "I love you, Grandma" === string {
+  else "I love you, Grandma" === string 
     return "I love you, too"
   }
 }
